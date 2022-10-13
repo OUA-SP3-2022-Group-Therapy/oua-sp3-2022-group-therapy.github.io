a0a3c9c91200c5b809e93c9b1931f9ea2c20bf34
AshNic86 <90290138+AshNic86@users.noreply.github.com>
2022-10-14 09:20:23 +1000

    lowercase folders
    Renamed files to fix error with CSS not loading in github.io/

---
+++ scripts/signUpProtoytype.js
@@ -0,0 +1,8 @@
+function showSignUp() {
+    var x = document.getElementById("overlaySignUpBlur");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
